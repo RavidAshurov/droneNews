71b40aac89b653daff3619dced215ec9f22c59b9
RavidAshurov <Ravid.Ashurov@indoor-robotics.com>
2024-11-05 15:43:03 +0200

    adjustments for client integ

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# droneNews
-Web application for drone related news curating
+# Drone News
+
+Web application for drone related news curating.
+
+The Application is constructed of 3 projects:
+
+   1. Api server (NodeJs + Express)
+   2. Client App (Vue)
+   3. Data collection microservice (NodeJs)
+
+Database used for this app is SQLite (chosen for the easy setup and the size and expected use of the data).
+
+Project was created on Node 23.1.0 environment.
+
+### Exec command (to be run from project root)
+
+```
+npm run start
+```
--- api-server/index.js
+++ api-server/index.js
@@ -1,14 +1,26 @@
 const express = require('express')
-const app = express();
-const headlinesController = require('./headlineController');
-const PORT = process.env.PORT || 3000;
+const app = express()
+const cors = require('cors')
+const headlinesController = require('./headlineController')
+const PORT = process.env.PORT || 3000
+
+app.use(
+  cors({
+    origin: [/0.0.0.0/, /localhost/],
+  })
+)
 
 app.get('/headlines', async function (req, res) {
-  console.log("Received GET request")
-  const recentHeadlines = await headlinesController.getHeadlines(req);
-  res.send(recentHeadlines)
+  try {
+    console.log('Received GET request')
+    const recentHeadlines = await headlinesController.getHeadlines(req)
+    res.send(recentHeadlines)
+  } catch (err) {
+    err.statusCode = 500
+    res.send(err)
+  }
 })
 
-app.listen(PORT, () => { 
-  console.log(`API Server is listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`API Server is listening on port ${PORT}`)
 })
